ce6d655ad12e534deca70281ce9ce09c52694c92
Nino Gubič <gubicnino@gmail.com>
2025-07-08 07:45:01 +0200

    update database for production

--- db/database.js
+++ db/database.js
@@ -1,12 +1,14 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host: 'prostovoljec-pb.mysql.database.azure.com',
-  user: 'student_admin',
-  password: 'Prostovoljec1',
-  database: 'ProjektnaDB',
-  port: 3306,
-  ssl: {}
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 connection.connect((err) => {
